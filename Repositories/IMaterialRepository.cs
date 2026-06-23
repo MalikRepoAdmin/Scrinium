2fb63e2295c10e35d5294e7aab7971bdebe06490
MalikRepoAdmin <mohamadmalikfajarbaihaqi@gmail.com>
2026-06-23 17:28:15 +0700

    refactor: Change Getter nullability and Simplify Deletion methods.
    Change the return of the getters into nullable and Simplify Deletion method because we will set Cascade deletion rules.

--- Repositories/IMaterialRepository.cs
+++ Repositories/IMaterialRepository.cs
@@ -4,17 +4,17 @@ using System.Threading.Tasks;
 
 public interface IMaterialRepository
 {
-    
     Task<IReadOnlyList<Material>> GetAllBySubjectAsync(Guid subjectId);
 
     Task<IReadOnlyList<Material>> GetAllByCollectionAsync(Guid collectionId);
 
-    Task<Material> GetByIdAsync(Guid id);
+    Task<Material?> GetByIdAsync(Guid id);
+
+    Task<IReadOnlyList<Material>> SearchAsync(string query, Guid? subjectId);
 
     Task AddAsync(Material material);
 
     Task UpdateAsync(Material material);
 
     Task DeleteAsync(Guid id);
-
 }
--- Repositories/ISubjectRepository.cs
+++ Repositories/ISubjectRepository.cs
@@ -4,15 +4,13 @@ using System.Threading.Tasks;
 
 public interface ISubjectRepository
 {
-    
     Task<IReadOnlyList<Subject>> GetAllByLibraryAsync(Guid libraryId);
 
-    Task<Subject> GetByIdAsync(Guid id);
+    Task<Subject?> GetByIdAsync(Guid id);
 
     Task AddAsync(Subject subject);
 
     Task UpdateAsync(Subject subject);
 
-    Task DeleteWithAllChildAsync(Guid id);
-
+    Task DeleteAsync(Guid id);
 }
