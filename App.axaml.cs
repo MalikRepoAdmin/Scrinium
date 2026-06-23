e6dbd78cf88d93c2055641cc0792102b7ef17f70
MalikRepoAdmin <mohamadmalikfajarbaihaqi@gmail.com>
2026-06-23 17:34:10 +0700

    refactor: Ensure Database have been created on startup

--- App.axaml.cs
+++ App.axaml.cs
@@ -1,12 +1,12 @@
+using System.Linq;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
-using System.Linq;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
 using Scrinium.ViewModels;
 using Scrinium.Views;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Scrinium;
 
@@ -19,15 +19,22 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
-
         var Services = DependencyInjection.ConfigureService();
+
+        // Ensure Db is created
+        using (var scope = Services.CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            db.Database.EnsureCreated();
+        }
 
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = Services.GetRequiredService<MainWindow>();
             {
                 DataContext = Services.GetRequiredService<MainWindowViewModel>();
-            };
+            }
+            ;
         }
 
         base.OnFrameworkInitializationCompleted();
